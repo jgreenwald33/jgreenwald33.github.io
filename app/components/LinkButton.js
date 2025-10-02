59d7e311e08727d76c9295e8d5d6ef794ef9fbbe
Jacob Greenwald <jakelgreenwald@gmail.com>
2025-10-01 21:40:44 -0500

    Added initial setup

--- app/components/LinkButton.js
+++
@@ -1,7 +0,0 @@
-export default function LinkButton({displayText, link, centered}) {
-    return (
-        <a href={"#" + link} className={"bg-black text-white py-2 px-4 rounded-full hover:bg-gray-900 no-underline text-center w-fit drop-shadow-lg hover:drop-shadow-2xl duration-500"}>
-            {displayText}
-        </a>
-    )
-}
